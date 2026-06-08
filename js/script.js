c4ee5958864d709c043058ad0d7b906665d8cecb
Nathaniel Misawo <nathaniel.misawo@strathmore.edu>
2026-06-08 10:44:13 +0300

    Update loops v2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-function greetings(fname) {
+function greetings(fname, today) {
     // Spot time
-    var today = new Date();
     var hour = today.getHours();
 
     // Greetings
@@ -27,6 +26,33 @@ function starTime() {
     var hour = today.getHours();
     var min = today.getMinutes();
     var sec = today.getSeconds();
-    document.getElementById("dateTime").innerHTML = dayName + ", " + day + " " + monthName + " " + year + " " + hour + ":" + min + ":" + sec;
+    document.getElementById("dateTime").innerHTML = dayName + ", " + day + " " + monthName + " " + year + " " + addZero(hour) + ":" + addZero(min) + ":" + addZero(sec);
     var t = setTimeout(starTime, 1000); // refresh time every 1 second
+}
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function daysInMonths(DateTime) { // function to calculate the number of days in the current month
+    var currentMonth = DateTime.getMonth() + 1; // get the current month (0-11, so we add 1 to get 1-12)
+    var months_with_30_days = [4, 6, 9, 11]; // April, June, September, November
+    var months_with_31_days = [1, 3, 5, 7, 8, 10, 12]; // January, March, May, July, August, October, December
+    var february_month = 2; // February
+    if (months_with_30_days.includes(currentMonth)) {
+        var daysInMonth = 30;
+    } else if (months_with_31_days.includes(currentMonth)) {
+        var daysInMonth = 31;
+    } else if (currentMonth === february_month) {
+        var year = DateTime.getFullYear(); // get the current year
+        // Check for leap year
+        if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+            var daysInMonth = 29; // Leap year
+        } else {
+            var daysInMonth = 28; // Non-leap year
+        }
+    }
+    return daysInMonth;
 }
